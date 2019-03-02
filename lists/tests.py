d6471317bef0261f7097ef73d3d918cdb87f4c5b
Bao Le <quybao@hotmail.com>
2019-03-01 16:18:11 -0800

    FT refactoring.

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,4 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-
-from lists.views import home_page
-
 
 class HomePageTest(TestCase):
 
@@ -14,4 +9,9 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
 
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
