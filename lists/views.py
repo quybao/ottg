cdfa86df218cb833dccc1c373be2875e5cef5f63
Bao Le <quybao@hotmail.com>
2019-03-05 14:13:24 -0800

    Move from Item.objects.create to Item() to remove the need to delete from auto save

--- lists/views.py
+++ lists/views.py
@@ -14,24 +14,22 @@ def view_list(request, list_id):
     error = None
     if request.method == 'POST':
         try:
-            item = Item.objects.create(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
-            item.delete()
     return render(request, 'list.html', {'list': list_, 'error': error})
 
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
         item.save()
     except ValidationError:
-        item.delete()
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
