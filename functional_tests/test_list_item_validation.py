8dc3aad66e9573e41b95988b71090bf300567be6
Bao Le <quybao@hotmail.com>
2019-03-06 16:38:37 -0800

    Add FT test to check on error being hiddlen when additional typing is done

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,8 +4,10 @@ from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
 
-    def test_cannot_add_empty_list_items(self):
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
 
+    def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
@@ -59,8 +61,27 @@ class ItemValidationTest(FunctionalTest):
 
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
         ))
 
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a list and cause some validation error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+        
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleased that the error goes away
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
