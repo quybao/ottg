74ac250cd063e5cffc85ad124e8432217639d6c7
Bao Le <quybao@hotmail.com>
2019-03-08 15:43:56 -0800

    Custom passwordless auth backend + custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -45,6 +45,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
+]
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',    
 ]
 
 MIDDLEWARE = [
@@ -127,6 +133,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'brianle@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 LOGGING = {
         'version' : 1,
--- superlists/urls.py
+++ superlists/urls.py
@@ -18,8 +18,10 @@ from django.urls import path
 from django.conf.urls import include
 from lists import views as list_views
 from lists import urls as list_urls
+from accounts import urls as account_urls
 
 urlpatterns = [
     path('', list_views.home_page, name='home'),
-    path('lists/', include(list_urls))
+    path('lists/', include(list_urls)),
+    path('accounts/', include(account_urls)),
 ]
